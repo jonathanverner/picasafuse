6793a402e876372cb36a21720f22c9208d49da71
Jonathan Livingston Verner <joni@galadriel.(none)>
2009-08-05 22:46:17 +0200

    Added a small testcase for parsing paths with authkey to pathparser.

--- testPathParser.cpp
+++ testPathParser.cpp
@@ -17,21 +17,23 @@
 
 bool testPathParser() { 
   pathParser t1("/"), t2("/jonathan.verner"), t3("/jonathan.verner/lide a zvirata"), t4("/jonathan.verner/lide a zvirata/joni2.jpg"),
-	     t5("//jonathan.verner");
+	     t5("//jonathan.verner"), t6("/jonathan.verner/test?authkey=ahoj");
   bool validityTest = true;
-  bool t1a = true, t2a = true, t3a = true, t4a = true, t5a = true;
-  bool t2b = true, t3b = true, t4b = true, t5b = true;
-  if ( !t1.isValid() || !t2.isValid() || !t3.isValid() || !t4.isValid() || !t5.isValid() ) validityTest=false;
+  bool t1a = true, t2a = true, t3a = true, t4a = true, t5a = true, t6a=true;
+  bool t2b = true, t3b = true, t4b = true, t5b = true, t6b = true;
+  if ( !t1.isValid() || !t2.isValid() || !t3.isValid() || !t4.isValid() || !t5.isValid() || !t6.isValid() ) validityTest=false;
   if ( t1.haveUser() || t1.haveAlbum() || t1.haveImage() ) t1a=false;
   if ( !t2.haveUser() || t2.haveAlbum() || t2.haveImage() ) t2a=false;
   if ( !t3.haveUser() || !t3.haveAlbum() || t2.haveImage() ) t3a=false;
   if ( !t4.haveUser() || !t4.haveAlbum() || !t4.haveImage() ) t4a=false;
   if ( !t5.haveUser() || t5.haveAlbum() || t5.haveImage() ) t5a=false;
+  if ( !t6.getType() == pathParser::ALBUM ) t6a=false;
   if ( t2.getUser() != "jonathan.verner" ) t2b=false;
   if ( t3.getUser() != "jonathan.verner" || t3.getAlbum() != "lide a zvirata" ) t3b=false;
   if ( t4.getUser() != "jonathan.verner" || t4.getAlbum() != "lide a zvirata" || t4.getImage() != "joni2.jpg" ) t4b=false;
   if ( t5.getUser() != "jonathan.verner" ) t5b=false;
-  if ( validityTest && t1a && t2a && t3a && t4a && t5a && t2b && t3b && t4b && t5b ) {
+  if ( t6.getUser() != "jonathan.verner" || t6.getAlbum() != "test?authkey=ahoj" ) t6b=false;
+  if ( validityTest && t1a && t2a && t3a && t4a && t5a && t6a && t2b && t3b && t4b && t5b && t6b) {
     std::cerr << "testPathParser PASSED\n";
     return true;
   }
