252504a2e45f804a51bcd273b223c1ef4c718c19
Jonathan Livingston Verner <joni@galadriel.(none)>
2009-08-05 22:53:19 +0200

    Added command line options to the picasaFUSE executable (to set username, cachedir etc.)

--- picasaFUSE.cpp
+++ picasaFUSE.cpp
@@ -1,12 +1,90 @@
 // main.cpp
 #include "picasaFS.h"
+#include <fuse/fuse.h>
 #include <string>
+#include <unistd.h>
+
+struct myfs_config {
+  char *cacheDir, *userName;
+  int updateInterval;
+};
+
+enum {
+  KEY_HELP,
+  KEY_VERSION,
+};
+
+const char PICASA_FUSE_VERSION[] = "v0.5";
+
+#define MYFS_OPT(t, p, v) { t, offsetof(struct myfs_config, p), v }
+
+static struct fuse_opt myfs_opts[] = {
+  MYFS_OPT("update-interval=%i",	updateInterval, 0),
+  MYFS_OPT("-t %i",             	updateInterval, 0),
+  MYFS_OPT("cachedir=%s",       	cacheDir, 0),
+  MYFS_OPT("username=%s",       	userName, 0),
+
+  FUSE_OPT_KEY("-V",             KEY_VERSION),
+  FUSE_OPT_KEY("--version",      KEY_VERSION),
+  FUSE_OPT_KEY("-h",             KEY_HELP),
+  FUSE_OPT_KEY("--help",         KEY_HELP),
+  NULL
+  //FUSE_OPT_END
+};
+
+static int myfs_opt_proc(void *data, const char *arg, int key, struct fuse_args *outargs)
+{
+  if ( key == KEY_HELP || key == KEY_VERSION ) { 
+    PicasaFS picasa;
+    switch (key) {
+      case KEY_HELP:
+	fprintf(stderr,
+	       "usage: %s mountpoint [options]\n"
+	       "\n"
+	       "general options:\n"
+	       "    -o opt,[opt...]  mount options\n"
+	       "    -h   --help      print help\n"
+	       "    -V   --version   print version\n"
+	       "\n"
+	       "PicasaFUSE options:\n"
+	       "    -o update-interval=NUM	update interval in seconds\n"
+	       "    -o cachedir=STRING		directory to store cached data\n"
+	       "    -o username=STRING		picasa username to authenticate as\n"
+	       "    -t NUM           		same as '-o update-interval=NUM'\n\n"
+	       , outargs->argv[0]);
+	fuse_opt_add_arg(outargs, "-ho");
+	picasa.main( outargs->argc, outargs->argv, NULL, &picasa );
+	exit(1);
+      case KEY_VERSION:
+	fprintf(stderr, "PicasaFUSE version %s\n", PICASA_FUSE_VERSION );
+	fuse_opt_add_arg(outargs, "--version");
+	picasa.main( outargs->argc, outargs->argv, NULL, &picasa );
+	exit(0);
+    }
+  }
+  return 1;
+}
+
 
 int main (int argc, char **argv) {
-        PicasaFS picasa;
-        // The first 3 parameters are identical to the fuse_main function.
-        // The last parameter gives a pointer to a class instance, which is
-        // required for static methods to access instance variables/ methods.
-        return picasa.main (argc, argv, NULL, &picasa);
+  struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+  struct myfs_config conf;
+  
+  memset(&conf, 0, sizeof(conf));
+  
+  fuse_opt_parse(&args, &conf, myfs_opts, myfs_opt_proc);
+  std::string userName = "",
+              password = "",
+	      cacheDir = "/tmp/.picasaFUSE/";
+  if ( conf.userName != NULL ) userName = conf.userName;
+  if ( conf.cacheDir != NULL ) cacheDir = conf.cacheDir;
+  if ( userName != "" ) password = getpass( "Enter picasa password:" );
+
+  PicasaFS picasa( userName, password, cacheDir, conf.updateInterval );
+	
+  // The first 3 parameters are identical to the fuse_main function.
+  // The last parameter gives a pointer to a class instance, which is
+  // required for static methods to access instance variables/ methods.
+  return picasa.main (args.argc, args.argv, NULL, &picasa);
 }
 
